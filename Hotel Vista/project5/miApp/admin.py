7de87b606c14f967dbc7cbc98716ce5779e4f4ae
Alexx3319 <alex.cordova@duocuc.cl>
2024-10-04 21:05:13 -0300

    v5

--- Hotel Vista/project5/miApp/admin.py
+++ Hotel Vista/project5/miApp/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import *
+from .models import informe, habitacion, reserva
 
 class MicuentaAdmin(admin.ModelAdmin):
-  readonly_fields = ('created', )
-  
-admin.site.register(informe,MicuentaAdmin)
+    readonly_fields = ('created',)
+
+    class Media:
+        css = {
+            'all': ('admin/css/custom_admin.css',)  # Añadir el CSS personalizado
+        }
+
+admin.site.site_header = "Hotel  Vista Administración"  # Cambia el título en la cabecera del panel
+admin.site.site_title = "Hotel  Vista Administración"  # Cambia el título del navegador
+admin.site.index_title = "Bienvenido a la Administración"
+
+admin.site.register(informe, MicuentaAdmin)
 admin.site.register(habitacion)
 admin.site.register(reserva)
